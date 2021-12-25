fc1fd10b027b7f7e2187ddc06836140ee3cc14a9
Alphaly <1990369867@qq.com>
2021-12-25 22:28:04 +0800

    init;

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by alphaly on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
